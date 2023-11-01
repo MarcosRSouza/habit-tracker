8db693cb7e115f43351410cea4a0b2919dc4e6ff
MarcosRSouza <marcosmrodrigues.souza@gmail.com>
2023-11-01 17:50:24 -0300

    feat (web): create initial template

---
+++ web/src/pages/Home/Home.js
@@ -0,0 +1,24 @@
+function Home() {
+
+    return (
+        <div>
+            <h2>My checklist</h2>
+            <ul>
+                <li>
+                    <input type="checkbox" id="lorem 1" />
+                    <label for="lorem1">Lorem 1</label>
+                </li>
+                <li>
+                    <input type="checkbox" id="lorem 2" />
+                    <label for="lorem2">Lorem 2</label>
+                </li>
+                <li>
+                    <input type="checkbox" id="lorem 3" />
+                    <label for="lorem3">Lorem 3</label>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default Home;
