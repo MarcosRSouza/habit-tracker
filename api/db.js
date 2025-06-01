1ee8d673a698ce9529c31327e89ac469939c160c
MarcosRSouza <marcosmrodrigues.souza@gmail.com>
2025-06-01 13:30:41 -0300

    restart project the simplest way possible

---
+++ api/db.js
@@ -0,0 +1,16 @@
+const { Pool } = require('pg');
+
+const dbUser = process.env.DATABASE_USER;
+const dbName = process.env.DATABASE_NAME;
+const dbDomain = process.env.DATABASE_DOMAIN;
+const dbPassword = process.env.DATABASE_PASSWORD;
+
+const pool = new Pool({
+  user: dbUser,
+  host: process.env.NODE_ENV === 'development' ? 'localhost' : dbDomain,
+  database: dbName,
+  password: dbPassword,
+  port: 5432, // padrão PostgreSQL
+});
+
+module.exports = pool;
---
+++ api/index.js
@@ -0,0 +1,40 @@
+// index.js
+require('dotenv').config();
+
+const port = parseInt(process.env.PORT);
+
+const express = require('express');
+const pool = require('./db');
+
+const app = express();
+app.use(express.json());
+
+// Exemplo: rota para listar clientes
+app.get('/users', async (req, res) => {
+  try {
+    const resultado = await pool.query('SELECT * FROM users');
+    res.json(resultado.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Erro ao buscar clientes');
+  }
+});
+
+app.get('/habits', async (req, res) => {
+  try {
+    const resultado = await pool.query('SELECT * FROM habits');
+    res.json(resultado.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Erro ao buscar hábitos');
+  }
+});
+
+// Rota de teste
+app.get('/', (req, res) => {
+  res.send('API funcionando!');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
